3eb878a0c87797168ac31adbd385983d4dab53d2
pepe <rizal27pangestu@gmail.com>
2022-03-10 20:42:03 +0700

    Tebak Angka

---
+++ src/Belajar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Belajar {
+
+    public static void main(String[] args) {
+        int random, tebak, jumlah;
+
+        random = (int) (Math.random() * 100);
+        System.out.println("Tebaklah Angka Antara 1-100");
+
+        Scanner masukan = new Scanner(System.in);
+        jumlah = 0;
+
+        do {
+            jumlah++;
+            System.out.println("Masukan Tebakan Anda : ");
+            tebak = masukan.nextInt();
+
+            if (jumlah != 5) {
+                if (tebak > random) {
+                    System.out.println("Tebakan terlalu Besar");
+                } else if (tebak < random) {
+                    System.out.println("Tebakan Terlalu Kecil");
+                } else {
+                    System.out.println("Tebakan Benar setelah " + jumlah + " Kali Menebak");
+                }
+            } else {
+                System.out.println("Anda Telah Salah Menebak Sebanyak 5 kali.\n Jawabannya adalah " + random);
+            }
+
+        } while (tebak != random && jumlah < 5);
+    }
+}
